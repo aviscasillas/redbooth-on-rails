8918640b491180d3a3b8a95a72f0c582b1dd7198
Albert Viscasillas <aviscasillas@gmail.com>
2015-01-31 13:37:42 +0100

    Sign in/out flow

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,5 @@
 class WelcomeController < ApplicationController
+  def index
+    redirect_to :projects if logged_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'auth/redbooth/callback', to: 'sessions#create'
+  get '/sign_out', to: 'sessions#destroy'
 
   root 'welcome#index'
   resources :projects
