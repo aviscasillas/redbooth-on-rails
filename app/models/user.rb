e3e617a76551ce6785becf9196c60a0b90867703
Albert Viscasillas <aviscasillas@gmail.com>
2015-02-02 09:02:51 +0100

    Fix oauth_expires_at to be datetime

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
       u.email = auth.info.email
       u.oauth_token = auth.credentials.token
       u.oauth_refresh_token = auth.credentials.refresh_token
-      u.oauth_expires_at = auth.credentials.expires_at
+      u.oauth_expires_at = Time.at(auth.credentials.expires_at)
       u.save!
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20150129205706) do
     t.string   "email"
     t.string   "oauth_token"
     t.string   "oauth_refresh_token"
-    t.string   "oauth_expires_at"
+    t.datetime "oauth_expires_at"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
   end
