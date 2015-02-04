3500e2976902ff079589be33cf009ca67d12858d
Albert Viscasillas <aviscasillas@gmail.com>
2015-02-04 09:39:03 +0100

    Start up with front end

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,33 @@
 module ApplicationHelper
+  def title(title)
+    content_tag(:h2) { title }
+  end
+
+  def default_description
+    content_tag(:em, class: 'text-muted') { 'No description available' }
+  end
+
+  def description(description)
+    text = description.present? ? description : default_description
+    text.html_safe
+  end
+
+  def created_on(datetime)
+    "Created on #{Time.at(datetime).strftime('%c')}"
+  end
+
+  def glyphicon(icon_name)
+    content_tag(:span, class: "glyphicon glyphicon-#{icon_name}") { '' }
+  end
+
+  def link_to_login(options = {})
+    text = "#{glyphicon('user')} Login with Redbooth".html_safe
+    link_to(text, '/auth/redbooth', options)
+  end
+
+  def link_to_logout
+    text =
+      "Logout <b>#{current_user.name}</b> #{glyphicon('log-out')}".html_safe
+    link_to(text, sign_out_path)
+  end
 end
