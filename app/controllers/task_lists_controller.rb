4b9fcfdab174122c1580890a37224b7162475560
Albert Viscasillas <aviscasillas@gmail.com>
2015-02-05 19:44:56 +0100

    Add task_list validation

--- app/controllers/task_lists_controller.rb
+++ app/controllers/task_lists_controller.rb
@@ -6,12 +6,20 @@ class TaskListsController < ApplicationController
   end
 
   def create
-    params_to_send = params.slice(:project_id).merge(params[:task_list])
-    redbooth.task_list.create(params_to_send)
-    redirect_to project_path(project_id)
+    @task_list = TaskList.new(task_list_params)
+    if @task_list.valid?
+      redbooth.task_list.create(task_list.to_provider)
+      redirect_to project_path(project_id)
+    end
+    flash.now[:error] = 'You have errors in the form'
+    render :new
   end
 
   private
+
+  def task_list_params
+    params.slice(:project_id).merge(params[:task_list])
+  end
 
   def project_id
     @project_id ||= params[:project_id]
